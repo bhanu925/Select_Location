2aec10a4ec77c9ec49c9167ffc4959d950abaf1e
bhanu925 <bhanu.chintu925@gmail.com>
2024-05-07 09:49:38 +0530

    code ready to deploy

---
+++ xstate/src/App.js
@@ -0,0 +1,11 @@
+import Location from "./LocationComponent/Location";
+
+function App() {
+  return (
+    <div className="App">
+     <Location/>
+    </div>
+  );
+}
+
+export default App;
---
+++ xstate/src/LocationComponent/Location.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import axios from 'axios';
+import { useState , useEffect } from 'react';
+import './Location.css';
+export default function Location() {
+
+   const [ countries , setCountries ] = useState([]);
+   const [ countryName , setCountryName ] = useState('')
+   const [ states , setStates ] = useState([]);
+   const [ stateName , setStateName ] = useState('')
+   const [ cities , setCities ] = useState([]);
+   const [ cityName , setCityName ] = useState('');
+   
+
+   useEffect(()=>{
+    const fetchCountries =async()=>{
+        try{
+            const response = await axios.get('https://crio-location-selector.onrender.com/countries')
+            setCountries(response.data);    
+        }
+        catch(err){
+            console.error("Error fetching the names",err)
+        }
+           
+    }
+    fetchCountries();
+   },[])
+
+   useEffect(()=>{
+    const fetchStates =async()=>{
+        if(countryName){
+            try{
+        const response = await axios.get(`https://crio-location-selector.onrender.com/country=${countryName}/states`)
+        setStates(response.data);
+        }      
+        catch(error){
+            console.error("Error fetching the names",error)
+        }     
+    }
+}
+    fetchStates();
+   },[countryName])
+
+   useEffect(()=>{
+    const fetchCities =async()=>{
+        if(stateName){
+            try{
+        const response = await axios.get(`https://crio-location-selector.onrender.com/country=${countryName}/state=${stateName}/cities`)
+        setCities(response.data);
+        }     
+        catch(error){
+            console.error("Error fetching the names",error)
+        }   
+    }    
+    }
+    fetchCities();
+   },[stateName])
+
+   return (
+   <div className="location">
+    <h1>Select Location</h1>
+
+    <select onChange={(e)=>setCountryName(e.target.value)} defaultValue={countryName}>
+        <option value={countryName} disabled>Select Country</option>
+        {countries.map((item,index)=>(
+            <option value={item} key={index}>{item}</option>
+        ))}
+    </select>
+
+    <select onChange={(e)=>setStateName(e.target.value)} defaultValue={stateName}>
+        <option value={stateName} disabled>Select State</option>
+        {states.map((item,index)=>(
+            <option value={item} key={index}>{item}</option>
+        ))}
+    </select>
+
+    <select onChange={(e)=>setCityName(e.target.value)} defaultValue={cityName}>
+        <option value={cityName} disabled>Select City</option>
+        {cities.map((item,index)=>(
+            <option value={item} key={index}>{item}</option>
+        ))}
+    </select>
+
+    {cityName && stateName && countryName &&
+    <h3>You selected {cityName} {stateName} {countryName}</h3>
+}
+   </div>)
+}
